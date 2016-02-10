9dc3b3e1e51b5e8cdde17474c0a17f0c1814ad6c
Skaanka <gripon.gregoire@sfr.fr>
2016-02-10 12:17:30 +0100

    first commit

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,4 @@
 <?php
 
 // Home page
-$app->get('/', "StartMySilex\Controller\HomeController::indexAction")->bind('link');
+$app->get('/', "StartMySilex\Controller\HomeController::indexAction")->bind('home');
